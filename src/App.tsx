2c74600fd8f7935e4f0a7368df644a659c93daea
Mmykron <>
2025-07-08 20:20:05 +0200

    Build 0.1.006 - Funkční frontend

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,30 @@
 import "./App.css";
+import { ConvexProvider, ConvexReactClient } from "convex/react";
 import { useQuery } from "convex/react";
 import { api } from "../convex/_generated/api";
 
-function App() {
+// DŮLEŽITÉ: ZADEJ SPRÁVNÝ Convex URL z tvého .env nebo dashboardu!
+const convex = new ConvexReactClient("https://dark-swan-999.convex.cloud");
+
+function TaskList() {
   const tasks = useQuery(api.tasks.get);
+  if (tasks === undefined) return <div>Načítání...</div>;
   return (
-    <div className="App">
-      {tasks?.map(({ _id, text }) => <div key={_id}>{text}</div>)}
+    <div>
+      {tasks.map(({ _id, text }: any) => <div key={_id}>{text}</div>)}
     </div>
   );
 }
 
-export default App;
+function App() {
+  return (
+    <ConvexProvider client={convex}>
+      <div className="App">
+        <h1>Seznam úkolů</h1>
+        <TaskList />
+      </div>
+    </ConvexProvider>
+  );
+}
 
+export default App;
